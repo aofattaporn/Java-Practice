19b4a94e17472a7800050c28b2782478978fb670
Attaporn Peungsook <aofattapon321@gmail.com>
2021-06-06 15:22:13 +0700

    learn chapter5 NumberAndString (6June2020)

---
+++ src/chapter5_NumberAndString/work5_1dNumberMethod.java
@@ -0,0 +1,12 @@
+package chapter5_NumberAndString;
+
+public class work5_1dNumberMethod {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE + "  ถึง  " + Integer.MAX_VALUE);
+
+        String s1 = "12345";
+        int i = Integer.parseInt(s1);
+        System.out.println(i);
+
+    }
+}
